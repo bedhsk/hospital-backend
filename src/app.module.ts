b810efa3d7defe34ca47788b69bc4ea04c24636c
cristian-siguenza16 <csiguenzavelasquez@gmail.com>
2024-08-24 21:44:36 -0600

    Agregado de modulos para correcto funcionamiento

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 import * as Joi from 'joi';
 
 @Module({
@@ -30,6 +31,7 @@ import * as Joi from 'joi';
       synchronize: false,
     }),
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,15 +1,21 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RolesController } from './roles/roles.controller';
 import { RolesService } from './roles/roles.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import User from './entities/user.entity';
 import Role from './entities/role.entity';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtGuard } from 'src/auth/jwt.guard';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Role])],
-  providers: [UsersService, RolesService],
-  controllers: [UsersController, RolesController]
+    imports: [TypeOrmModule.forFeature([User, Role])],
+    controllers: [UsersController, RolesController],
+    providers: [UsersService, RolesService, {
+        provide: APP_GUARD, 
+        useClass: JwtGuard,
+    }],
+    exports: [UsersService],
 })
 export class UsersModule {}
