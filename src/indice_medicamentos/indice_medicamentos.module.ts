f6cadbde96ee7a51a8f940e64aa45506f2fc4965
MarcoJav15 <marcodeleon2002@gmail.com>
2024-08-26 23:12:48 -0600

    Arreglando bug

--- src/indice_medicamentos/indice_medicamentos.module.ts
+++ src/indice_medicamentos/indice_medicamentos.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { IndiceMedicamentosService } from './indice_medicamentos.service';
 import { IndiceMedicamentosController } from './indice_medicamentos.controller';
+import { Type } from 'class-transformer';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import IndiceMedicamento from './entities/indice_medicamento.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([IndiceMedicamento])],
   providers: [IndiceMedicamentosService],
   controllers: [IndiceMedicamentosController]
 })
--- src/medicamentos/medicamentos.module.ts
+++ src/medicamentos/medicamentos.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MedicamentosService } from './medicamentos.service';
 import { MedicamentosController } from './medicamentos.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import Medicamento from './entities/medicamento.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Medicamento])],
   providers: [MedicamentosService],
   controllers: [MedicamentosController]
 })
