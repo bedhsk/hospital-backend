be0737e741aa6ca2fc5273afd30bbb63e9ea0c00
urruthia <urrutiapedro494gmail.com>
2024-09-08 22:42:13 -0600

    Creaciòn de dtos para pacientes

---
+++ src/pacientes/dto/create-paciente.dto.ts
@@ -0,0 +1,62 @@
+import { Type } from "class-transformer";
+import { IsDate,IsIn,IsOptional, IsString, MaxLength, MinLength } from "class-validator";
+
+class CreatePacienteDto {
+
+    @IsString()
+    @MinLength(1)
+    @MaxLength(60)
+    nombre: string;
+
+    @IsString()
+    @IsIn(['Masculino', 'Femenino'])
+    @MaxLength(10)
+    sexo: string;
+
+    @IsString()
+    @MaxLength(13)
+    @IsOptional()
+    cui?: string;
+    
+    @Type(() => Date) 
+    @IsDate()
+    nacimiento: Date;
+    
+    @IsString()
+    @MinLength(1)
+    @MaxLength(255)
+    @IsOptional()
+    familiares?: string;
+
+    @IsString()
+    @MinLength(1)
+    @MaxLength(255)
+    @IsOptional()
+    medicos?: string;   
+
+    @IsString()
+    @MinLength(1)
+    @MaxLength(255)
+    @IsOptional()
+    quirurgicos?: string;
+
+    @IsString()
+    @MinLength(1)
+    @MaxLength(255)
+    @IsOptional()
+    traumaticos?: string;
+
+    @IsString()
+    @MinLength(1)
+    @MaxLength(255)
+    @IsOptional()
+    alergias?: string;
+
+    @IsString()
+    @MinLength(1)
+    @MaxLength(255)
+    @IsOptional()
+    vicios?: string;
+}
+
+export default CreatePacienteDto;
