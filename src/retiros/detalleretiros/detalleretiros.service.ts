6c84a175bccf8f473a3c3471cb04fb7809d93c13
urruthia <urrutiapedro494gmail.com>
2024-10-01 22:57:25 -0600

    Creaciòn de Crud Detalle Servicio

--- src/retiros/detalleretiros/detalleretiros.service.ts
+++ src/retiros/detalleretiros/detalleretiros.service.ts
@@ -31,11 +31,28 @@ export class DetalleretirosService {
         return detalleAdquisicion;
       }
 
+      async findOneByRetiroId(id: string) {
+        const detalleRetiro = await this.detalleRetiroRepository.findOne({
+          where: {
+            retiro: { id },
+            is_active: true,
+          },
+          relations: ['retiro', 'insumoDepartamento'],
+        });
+      
+        if (!detalleRetiro) {
+          throw new NotFoundException(
+            `Insumo departamento con ID de retiro  ${id} no encontrado o desactivado`,
+          );
+        }
+        return detalleRetiro;
+      }
+
       async create(createDetalleRetiro: CreateDetalleRetiroDto) {
         const { retiroId, insumoDepartamentoId, ...rest } = createDetalleRetiro;
         
         const insumoDepartamento = await this.insumoDepartamentosService.findOne(
-            createDetalleRetiro.insumoDepartamentoId,
+          createDetalleRetiro.insumoDepartamentoId,
         );
     
         if (!insumoDepartamento) {
@@ -44,10 +61,14 @@ export class DetalleretirosService {
           );
         }
     
-        // Crear el nuevo detalle retiro con sus respectivas relaciones
+        
         const detalleAdquisicion = this.detalleRetiroRepository.create({
           ...rest,
-          insumoDepartamento, // Relacionar el detalleadquisicion con el insumoDepartamento encontrado
+          retiro: {id: retiroId},
+          insumoDepartamento: {
+            id: insumoDepartamento.id,
+            existencia: insumoDepartamento.existencia,
+          }, // Relacionar el detalleadquisicion con el insumoDepartamento encontrado
         });
     
         return await this.detalleRetiroRepository.save(detalleAdquisicion);
@@ -55,27 +76,33 @@ export class DetalleretirosService {
 
  
         // Actualizar un detalle retiro existente
-  async update(id: string, updateDetalleRetiroDto: UpdateDetalleRetiroDto) {
-    const detalleAdquisicion = await this.findOne(id);
-    if (!detalleAdquisicion) {
-      throw new NotFoundException(
-        `Detalle reitro con ID ${id} no encontrado o desactivado`,
-      );
-    }
-    
-    const insumoDepartamento = await this.insumoDepartamentosService.findOne(
-        updateDetalleRetiroDto.insumoDepartamentoId,
-    );
-    
-    if (!insumoDepartamento) {
-      throw new NotFoundException(
-        `Insumo departamento con id ${updateDetalleRetiroDto.insumoDepartamentoId} no encontrado`,
-      );
-    }
-
-    this.detalleRetiroRepository.merge(detalleAdquisicion, updateDetalleRetiroDto);
-    return await this.detalleRetiroRepository.save(detalleAdquisicion);
-  }
+        async update(id: string, updateDetalleRetiro: UpdateDetalleRetiroDto) {
+          const detalleRetiro = await this.findOne(id);
+          if (!detalleRetiro) {
+            throw new NotFoundException(
+              `Detalle Retiro  con ID ${id} no encontrado o desactivado`,
+            );
+          }
+          
+          const insumoDepartamento = await this.insumoDepartamentosService.findOne(
+            detalleRetiro.insumoDepartamento.id,
+          );
+          
+          if (!insumoDepartamento) {
+            throw new NotFoundException(
+              `Insumo departamento con id ${updateDetalleRetiro.insumoDepartamentoId} no encontrado`,
+            );
+          }
+          await this.insumoDepartamentosService.update(
+            insumoDepartamento.id,
+            {
+              existencia: insumoDepartamento.existencia  - updateDetalleRetiro.cantidad + detalleRetiro.cantidad
+            }
+          );
+      
+          this.detalleRetiroRepository.merge(detalleRetiro, {cantidad: updateDetalleRetiro.cantidad});
+          return await this.detalleRetiroRepository.save(detalleRetiro);
+        }
 
   // Realiza el soft delete cambiando el campo is_active a false
   async softDelete(id: string) {
