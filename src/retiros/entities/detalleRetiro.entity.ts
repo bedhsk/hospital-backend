1f2102a70dcf994b12b79a14f2057ee2b1317740
Brian <brianbatz03@gmail.com>
2024-10-12 02:23:41 -0600

    fix(retiro): :bug: Arreglando errores en retiros
    Errores al generar el retiro debido al uuid no se generaba automáticamente, y errores con la cantidad en detalle retiro, estaba definida como booleana

--- src/retiros/entities/detalleRetiro.entity.ts
+++ src/retiros/entities/detalleRetiro.entity.ts
@@ -1,47 +1,55 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsNumber } from "class-validator";
-import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn, UpdateDateColumn } from "typeorm";
-import Retiro from "./retiro.entity";
-import { InsumoDepartamento } from "src/insumo_departamentos/entities/insumo_departamento.entity";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber } from 'class-validator';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import Retiro from './retiro.entity';
+import { InsumoDepartamento } from 'src/insumo_departamentos/entities/insumo_departamento.entity';
 
 @Entity('detalleRetiro')
-export default  class DetalleRetiro{
+export default class DetalleRetiro {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @PrimaryColumn('uuid')
-    id: string;
+  @Column({ default: true })
+  @ApiProperty({
+    description:
+      'Muestra si el detalle retiro se encuetra activo (sirve para el SoftDelete)',
+  })
+  is_active: boolean;
 
-    @Column({ default: true})
-    @ApiProperty({
-      description: 'Muestra si el detalle retiro se encuetra activo (sirve para el SoftDelete)',
-    })
-    is_active: boolean;
+  @CreateDateColumn({ name: 'created_at', type: 'timestamp with time zone' })
+  @ApiProperty({
+    description:
+      'Fecha de Creaciòn del detalle retiro se inserta automaticamente',
+  })
+  createdAt: Date;
 
-    @CreateDateColumn({ name: 'created_at', type: 'timestamp with time zone' })
-    @ApiProperty({
-      description: 'Fecha de Creaciòn del detalle retiro se inserta automaticamente',
-    })
-    createdAt: Date;
-  
-    @UpdateDateColumn({ name: 'updated_at', type: 'timestamp with time zone' })
-    @ApiProperty({
-      description:
-        'Ultima fecha de modificaciòn del detalle retiro',
-    })
-    updatedAt: Date;
+  @UpdateDateColumn({ name: 'updated_at', type: 'timestamp with time zone' })
+  @ApiProperty({
+    description: 'Ultima fecha de modificaciòn del detalle retiro',
+  })
+  updatedAt: Date;
 
-    @Column({ type: 'boolean', default: true })
-    @ApiProperty({   description:
-        'Cantidad retirada',})
-    @IsNumber()
-    cantidad: number; 
+  @Column({ type: 'int' })
+  @ApiProperty({ description: 'Cantidad retirada' })
+  @IsNumber()
+  cantidad: number;
 
-    @ManyToOne(() => Retiro, (retiro) => retiro.detalleRetiro)
-    @JoinColumn({ name: 'retiroId' })
-    retiro: Retiro; 
+  @ManyToOne(() => Retiro, (retiro) => retiro.detalleRetiro)
+  @JoinColumn({ name: 'retiroId' })
+  retiro: Retiro;
 
-    @ManyToOne(() => InsumoDepartamento, (insumoDepartamento) => insumoDepartamento.lotes,)
-    @JoinColumn({ name: 'insumoDepartamentoId' })
-    insumoDepartamento: InsumoDepartamento;
+  @ManyToOne(
+    () => InsumoDepartamento,
+    (insumoDepartamento) => insumoDepartamento.lotes,
+  )
+  @JoinColumn({ name: 'insumoDepartamentoId' })
+  insumoDepartamento: InsumoDepartamento;
 }
-
-
