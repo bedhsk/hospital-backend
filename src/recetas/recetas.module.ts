e7b5fefdc0773175fc85324e6c2dba68729ba559
Brian <brianbatz03@gmail.com>
2024-10-19 19:33:05 -0600

    fix(recetas): :bug: Dependencia de recetas agregada

--- src/recetas/recetas.module.ts
+++ src/recetas/recetas.module.ts
@@ -9,10 +9,20 @@ import { UsersService } from 'src/users/users.service';
 import { PacientesService } from 'src/pacientes/pacientes.service';
 import { RolesService } from 'src/users/roles/roles.service';
 import Role from 'src/users/entities/role.entity';
+import Departamento from 'src/departamentos/entities/departamento.entity';
+import { DepartamentosService } from 'src/departamentos/departamentos.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Receta, User, Paciente, Role])],
+  imports: [
+    TypeOrmModule.forFeature([Receta, User, Paciente, Role, Departamento]),
+  ],
   controllers: [RecetasController],
-  providers: [RecetasService, UsersService, PacientesService, RolesService],
+  providers: [
+    RecetasService,
+    UsersService,
+    PacientesService,
+    RolesService,
+    DepartamentosService,
+  ],
 })
 export class RecetasModule {}
