04dafa001a3fce567a642f27c45d312f5ce6c02d
Brian <brianbatz03@gmail.com>
2024-10-25 16:01:36 -0600

    fix(orden_laboratorioos): :bug: Arreglando errores de dependencias

--- src/orden_laboratorios/entities/orden_laboratorio.entity.ts
+++ src/orden_laboratorios/entities/orden_laboratorio.entity.ts
@@ -1,49 +1,74 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 import Paciente from '../../pacientes/entities/paciente.entity';
 import Examen from '../../examenes/entities/examen.entity';
 import User from '../../users/entities/user.entity';
-import Retiro from '../../retiros/entities/retiro.entity';
 import { ApiProperty } from '@nestjs/swagger';
-import { IsUUID, IsString, IsBoolean } from 'class-validator';
+import { IsString, IsBoolean } from 'class-validator';
 import { EstadoOrdenLaboratorio } from '../enum/estado-orden-laboratorio.enum';
+import Retiro from 'src/retiros/entities/retiro.entity';
 
 @Entity('ordenLaboratorio')
 class OrdenLaboratorio {
-    @PrimaryGeneratedColumn('uuid')
-    @ApiProperty()
-    id: string;
+  @PrimaryGeneratedColumn('uuid')
+  @ApiProperty()
+  id: string;
 
-    @Column({ type: 'text' })
-    @ApiProperty()
-    @IsString()
-    descripcion: string;
+  @Column({ type: 'text' })
+  @ApiProperty()
+  @IsString()
+  descripcion: string;
 
-    @Column({ type: 'enum', enum: EstadoOrdenLaboratorio, default: EstadoOrdenLaboratorio.PENDIENTE })
-    @ApiProperty({
-        description: 'Estado de la orden de laboratorio',
-        enum: EstadoOrdenLaboratorio,
-        example: EstadoOrdenLaboratorio.PENDIENTE,
-    })
-    estado: EstadoOrdenLaboratorio;
-    
+  @Column({
+    type: 'enum',
+    enum: EstadoOrdenLaboratorio,
+    default: EstadoOrdenLaboratorio.PENDIENTE,
+  })
+  @ApiProperty({
+    description: 'Estado de la orden de laboratorio',
+    enum: EstadoOrdenLaboratorio,
+    example: EstadoOrdenLaboratorio.PENDIENTE,
+  })
+  estado: EstadoOrdenLaboratorio;
 
-    @ManyToOne(() => User, (usuario) => usuario.ordenesLaboratorio)
-    usuario: User;
+  @CreateDateColumn({ name: 'created_at', type: 'timestamp with time zone' })
+  @ApiProperty({
+    description: 'Fecha de Creación de la receta, se inserta automáticamente',
+  })
+  created_at: Date;
 
-    @ManyToOne(() => Retiro, (retiro) => retiro.ordenesLaboratorio, { nullable: true })
-    retiro: Retiro;
+  @UpdateDateColumn({ name: 'updated_at', type: 'timestamp with time zone' })
+  @ApiProperty({
+    description: 'Última fecha de modificación de la receta',
+  })
+  updated_at: Date;
 
-    @ManyToOne(() => Paciente, (paciente) => paciente.ordenesLaboratorio)
-    paciente: Paciente;
+  @ManyToOne(() => User, (usuario) => usuario.ordenesLaboratorio)
+  usuario: User;
 
-    @ManyToOne(() => Examen, (examen) => examen.ordenesLaboratorio, { nullable: false })
-    examen: Examen;
+  @ManyToOne(() => Retiro, (retiro) => retiro.ordenesLaboratorio, {
+    nullable: true,
+  })
+  retiro: Retiro;
 
+  @ManyToOne(() => Paciente, (paciente) => paciente.ordenesLaboratorio)
+  paciente: Paciente;
 
-    @Column({ type: 'boolean', default: true })
-    @ApiProperty()
-    @IsBoolean()
-    is_active: boolean = true;
+  @ManyToOne(() => Examen, (examen) => examen.ordenesLaboratorio, {
+    nullable: false,
+  })
+  examen: Examen;
+
+  @Column({ type: 'boolean', default: true })
+  @ApiProperty()
+  @IsBoolean()
+  is_active: boolean = true;
 }
 
 export default OrdenLaboratorio;
