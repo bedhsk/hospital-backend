3b06f7510edf7d385d2de93bfed6da81b2e867e4
Brian <brianbatz03@gmail.com>
2024-11-27 19:29:19 -0600

    feat(Auth): :ambulance: Corrigiendo exepción enviada al hacer una acción no autorizada

--- src/auth/role.guard.ts
+++ src/auth/role.guard.ts
@@ -1,4 +1,9 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
 import Role from 'src/users/entities/role.entity';
@@ -21,12 +26,21 @@ export class RoleGuard implements CanActivate {
     const { user } = context.switchToHttp().getRequest();
 
     // Allow access to SuperUser
-    const superUser = user?.role?.name === 'SuperAdmin';
+    const superUser =
+      user?.role?.name === 'SuperAdmin' || user?.role?.name === 'Dirección';
     if (superUser) {
       return true;
     }
 
     // Otherwise, check if the user's role matches one of the required roles
-    return requiredRoles.includes(user?.role?.name);
+    const match = requiredRoles.includes(user?.role?.name);
+
+    if (!match) {
+      throw new UnauthorizedException(
+        'No tienes permisos para realizar esta acción',
+      );
+    }
+
+    return match; // true if the user's role matches one of the required roles
   }
 }
